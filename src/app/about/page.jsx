b0b54134bf47e561210ac5949132522bedbf9513
unknown <shah.sid.ktm@gmail.com>
2023-06-26 10:17:41 +0545

    Learning git

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
-
+import style from './page.module.css'
+import Button from '@/components/Button/Button'
 const About = () => {
   return (
-    <div> About</div>
+    <div className={style.container}>
+      <div className={style.imageContainer}>
+        <Image
+        src="https://images.pexels.com/photos/1926988/pexels-photo-1926988.jpeg?cs=srgb&dl=pexels-ricky-esquivel-1926988.jpg&fm=jpg&_gl=1*1w7pgna*_ga*MzU4MDEzNTUzLjE2ODczNDY5NDQ.*_ga_8JE65Q40S6*MTY4NzUxNTk2MC4zLjEuMTY4NzUxNTk5MS4wLjAuMA.."
+        fill={true}
+        className={style.img}
+        />
+        <div className={style.imageText}>
+          <h1 className={style.imageTitle}>Digital Story Teller</h1>
+          <h2 className={style.imageDesc}>Handicraft award winning</h2>
+        </div>
+      </div>
+      <div className={style.textContainer}>
+        <div className={style.item}>
+          <h1>Who are we?</h1>
+          <p className={style.desc}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+           when an unknown printer took a galley of type and scrambled it to make a 
+           type specimen book. It has survived not only five centuries, but also the
+           <br/>
+           <br/>
+            leap into electronic typesetting, remaining essentially unchanged. It was
+           popularised in the 1960s with the release of Letraset sheets containing Lorem 
+            Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
+           including versions of Lorem Ipsum.
+          </p>
+        </div>
+        <div className={style.item}></div>
+        <div className={style.title}>What we do?</div>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+           when an unknown printer took a galley of type and scrambled it to make a 
+           type specimen book. It has survived not only five centuries, but also the
+           <br/>
+           <br/>
+           - Dynamic Website fast and handy mobile
+        </p>
+        <Button url="/contact" text="Contact"/>
+      </div>
+    </div>
   )
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Button from '@/components/Button/Button'
 
 export default function Home() {
   return (
@@ -8,11 +9,11 @@ export default function Home() {
       <div className={styles.items}>
         <h1 className={styles.title}>Better Design For your digital products</h1>
         <p className={styles.desc}>Turning Your Idea into Reality. We bring together the teams from the global tech industry</p>
-        <button className={styles.button}>See our works</button>
+        <Button url="#" text="See Our Works"/>
       </div>
 
       <div className={styles.items}>
-      <Image src="https://images.pexels.com/photos/39853/woman-girl-freedom-happy-39853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image" width={500} height={500}/>
+      <Image className={styles.img} src="https://images.pexels.com/photos/39853/woman-girl-freedom-happy-39853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image" width={500} height={500}/>
       </div>
 
     </div>
